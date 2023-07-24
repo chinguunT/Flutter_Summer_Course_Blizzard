8ffb50723f34659623bd9c452d4e742b6986fc9f
Blizzard-3 <blizzard-3@Admin-3s-iMac.local>
2023-07-24 11:34:15 +0800

    Immutable widget

---
+++ day13/lib/basic_screen.dart
@@ -0,0 +1,34 @@
+import 'package:day13/immutable_widget.dart';
+import 'package:flutter/material.dart';
+
+class BasicScreen extends StatelessWidget {
+  const BasicScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: const Text('My flutter app'),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Icon(Icons.edit,),
+          ),
+
+        ],
+      ),
+
+      body: const Center(child: AspectRatio(aspectRatio: 1.0, child: ImmutableWidget(),)),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.lightBlue,
+          child: const Center(
+            child: Text('i am a Drawer'),
+          )
+        ),
+      ),
+    );
+  }
+}
+
---
+++ day13/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:day13/basic_screen.dart';
+import 'package:day13/immutable_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: BasicScreen(),
+  ));
+}
