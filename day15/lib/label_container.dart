c5fd94277ebb4263cecc580c6856daa7fc317af3
Blizzard-3 <blizzard-3@Admin-3s-iMac.local>
2023-07-28 11:03:47 +0800

    day 15 flexible and expanded

---
+++ day15/lib/label_container.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LabeledContainer extends StatelessWidget {
+  LabeledContainer({Key? key, this.width, this.height = double.infinity, required this.color, required this.text, this.textColor}): super(key: key);
+
+  double? width;
+  double? height;
+  final Color? color;
+  final String text;
+  Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      color: color,
+      child: Center(
+        child: Text(text, style: TextStyle(color: textColor, fontSize: 20),),
+      ),
+    );
+  }
+}
---
+++ day15/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:day15/flex_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: FlexScreen(),
+  ));
+}
